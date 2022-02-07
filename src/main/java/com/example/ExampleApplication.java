5498cbf5025731fd788f3983df189aaf11101c2a
Shubham Kumar <shubhamkumar3183@gmail.com>
2022-02-07 17:47:01 +0530

    new files added

---
+++ src/main/java/com/example/ExampleApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.controller.UserController;
+import com.example.entity.Users;
+import com.example.repository.UserRepository;
+import com.example.service.UserService;
+import com.exapmle.service.impl.UserServiceImpl;
+
+@SpringBootApplication(scanBasePackageClasses = {UserController.class, UserService.class, UserServiceImpl.class,Users.class, UserRepository.class})
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+}
